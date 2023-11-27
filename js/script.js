071cd59c5ada7d7552e8a8df6d2934a93ed52a17
gds90 <you@example.com>
2023-11-27 14:24:54 +0100

    Inserito script in script.js

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// definisco un'array con la lista delle cose da acquistare
+const list = ['Sale', 'Pomodori', 'Cipolle', 'Burro', 'Bagnoschiuma', 'Pasta', 'Olio EVO', 'Patatine', 'Bibite'];
+
+// associo l'elemento HTML con id 'shopping_list' ad una variabile
+const shopping_list = document.getElementById('shopping_list');
+
+// inizializzo la variabile contatore
+let i = 0;
+
+// tramite un ciclo while stampo la lista in DOM
+while (i<list.length){
+
+    // definisco una variabile in cui creo un elemento della lista
+    const li = document.createElement('li');
+    
+    // alla variabile precedentemente creata associo il valore dell'elemento i dell'array
+    li.innerText = list[i];
+
+    // aggiungo l'elemento alla lista in HTML tramite la funzione .appendChild
+    shopping_list.appendChild(li);
+
+    // incremento della variabile contatore
+    i++;
+}
